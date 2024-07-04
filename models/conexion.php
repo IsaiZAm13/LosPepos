ae4d4fdc49389c2e9e764d00870d9fd33a806ebe
IsaiZAm13 <155692854+IsaiZAm13@users.noreply.github.com>
2024-07-04 03:39:33 +0000

    Se agrega conexiones

--- models/conexion.php
+++ models/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public static function conectar(){
 
-	    $link = new PDO("mysql:host=localhost;dbname=tecni", "root", "");
+	    $link = new PDO("mysql:host=db;dbname=mariadb", "mariadb", "mariadb");
 	    return $link;
 
     }
